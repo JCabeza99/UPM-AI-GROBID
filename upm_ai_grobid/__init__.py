cd4de77fb48768645bcd1a13d93835d16d26796d
JCabeza99 <jaime.cabeza99@gmail.com>
2023-03-05 19:22:51 +0100

    JaCa:word cloud generation tested

--- upm_ai_grobid/__init__.py
+++ upm_ai_grobid/__init__.py
@@ -1,1 +1,38 @@
+import os
+import glob
+import requests
+import xml.etree.ElementTree as ET
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
+
+def processFile(pdf_file):
+    with open(pdf_file, "rb") as data:
+        response = requests.post(grobid_url, files={'input': data})
+        return response
+
+
+def getAbstract(root):
+    for elemento in root.iter():
+        if (elemento.tag == "{http://www.tei-c.org/ns/1.0}abstract"):
+            result = elemento.find("{http://www.tei-c.org/ns/1.0}p")
+            return result.text
+
+
+# Set grobid URL
+grobid_url = "http://localhost:8070/api/processHeaderDocument"
+
+# Search for any PDF file in the input folder
+input_path = "INPUT/"
+
+# Get all the PDF files inside the input folder
+pdf_files = glob.glob(os.path.join(input_path, "*.pdf"))
+
+
+for pdf_file in pdf_files:
+    response = processFile(pdf_file)
+    root = ET.fromstring(response.content)
+    abstract = getAbstract(root)
+    wordcloud = WordCloud().generate(abstract)
+    plt.imshow(wordcloud)
+    plt.savefig("img")
