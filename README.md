385029314050bfc3cd679c1193a5d744661948a1
JCabeza99 <jaime.cabeza99@gmail.com>
2023-03-07 20:04:22 +0100

    JaCa: script finished

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # UPM-AI-GROBID
+
+
+cff codemeta read the docs y zeno workflow
+
+sudo apt-get install wkhtmltopdf
--- upm_ai_grobid/__init__.py
+++ upm_ai_grobid/__init__.py
@@ -1,9 +1,14 @@
 import os
 import glob
 import requests
+import pdfkit
+import jinja2 
+import io
+import base64
 import xml.etree.ElementTree as ET
 import matplotlib.pyplot as plt
-from wordcloud import WordCloud
+from wordcloud import WordCloud, STOPWORDS
+from datetime import datetime
 
 
 def processFile(pdf_file):
@@ -12,11 +17,29 @@ def processFile(pdf_file):
         return response
 
 
-def getAbstract(root):
-    for elemento in root.iter():
-        if (elemento.tag == "{http://www.tei-c.org/ns/1.0}abstract"):
-            result = elemento.find("{http://www.tei-c.org/ns/1.0}p")
-            return result.text
+def getAbstract(root: ET.Element):
+    for element in root.iter():
+        if (element.tag == "{http://www.tei-c.org/ns/1.0}abstract"):
+            for abstractElem in element.iter():
+                if (abstractElem.tag == '{http://www.tei-c.org/ns/1.0}p'):
+                    return abstractElem.text
+
+def getfigures(root: ET.Element):
+    result = 0
+    for element in root.iter():
+        if(element.tag == "{http://www.tei-c.org/ns/1.0}figure"):
+            result += 1
+    return result
+
+def getLinks(root: ET.Element):
+    links = []
+    for element in root.iter():
+        if(element.tag == "{http://www.tei-c.org/ns/1.0}ptr"):
+            links.append(element.attrib["target"])
+    return  '<br>- '.join(links)
+
+    
+
 
 
 # Set grobid URL
@@ -28,10 +51,72 @@ input_path = "INPUT/"
 # Get all the PDF files inside the input folder
 pdf_files = glob.glob(os.path.join(input_path, "*.pdf"))
 
+# Hold the number of figures per article
+figures = []
+paper = 1
+x_axis = []
+
+# stopwords for the wordcloud generation
+stopwords = STOPWORDS
+stopwords.add('et')
+stopwords.add('al')
+
+# load templates
+template_loader = jinja2.FileSystemLoader('./Template')
+template_env = jinja2.Environment(loader=template_loader)
+report_template = template_env.get_template('report.html')
+histogram_template = template_env.get_template('histogram.html')
+config = pdfkit.configuration(wkhtmltopdf='/usr/bin/wkhtmltopdf')
+output = ""
+
+# get date and time of fenerated time
+now = datetime.now()
+
 
 for pdf_file in pdf_files:
+    x_axis.append(f"paper_{paper}")
+    paper += 1
     response = processFile(pdf_file)
     root = ET.fromstring(response.content)
     abstract = getAbstract(root)
-    wordcloud = WordCloud().generate(abstract)
     
+    generated_wordcloud = WordCloud(width= 700, height= 400,stopwords=stopwords, background_color='white', min_font_size=10).generate(abstract)
+    
+    figures.append(getfigures(root))
+    
+    
+    obtainedLinks = getLinks(root)
+    
+    if(obtainedLinks == ""):
+        obtainedLinks = "No link were found in this paper<br>"
+    else :
+        obtainedLinks = "- " + obtainedLinks
+    
+    wordcloud_image_name =  f"paper_{paper}.png"
+    generated_wordcloud.to_image().save(wordcloud_image_name)
+
+    
+    content= {'title' : os.path.basename(pdf_file).split(".")[0], 'image': f"./{wordcloud_image_name}", 'links': obtainedLinks}
+    
+    output += "\n" + report_template.render(content)
+    
+# Create the histogram plot
+plt.bar(x_axis, figures)
+plt.title('Histograma de las figuras encontradas')
+
+figure_name = "figure.png"
+plt.savefig(figure_name)
+
+histogram_content = {'image' : f"./{figure_name}"}
+
+output += histogram_template.render(histogram_content)
+
+with open("output.html", "w") as html:
+    html.write(output)
+    
+pdfkit.from_file("output.html", f"report-{now}.pdf", configuration= config, options={"enable-local-file-access": ""})
+
+
+    
+    
+
