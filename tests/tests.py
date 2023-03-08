c33221713bf777886c7763950e201bd0b0161da2
JCabeza99 <jaime.cabeza99@gmail.com>
2023-03-08 23:50:00 +0100

    JaCa:almost last commit

--- tests/tests.py
+++ tests/tests.py
@@ -1,2 +1,28 @@
 import unittest
-    
+from upm_ai_grobid import mama_papers
+import xml.etree.ElementTree as ET
+
+
+class TestProject(unittest.TestCase):
+    def test_getAbstract(self):
+        with open("test.xml") as test:
+            self.assertNotEqual(mama_papers.getAbstract(ET.parse(test).getroot()),"")
+    def test_getFigures(self):
+        with open("test.xml") as test:
+            self.assertEqual(mama_papers.getFigures(ET.parse(test).getroot()),0)
+    def test_getLinks(self):
+        with open("test.xml") as test:
+            self.assertEqual(mama_papers.getLinks(ET.parse(test).getroot()),"") 
+    def test_getAbstract2(self):
+        with open("test1.xml") as test:
+            self.assertEqual(mama_papers.getAbstract(ET.parse(test).getroot()),"")
+    def test_getFigures2(self):
+        with open("test1.xml") as test:
+            self.assertEqual(mama_papers.getFigures(ET.parse(test).getroot()),0)
+    def test_getLinks2(self):
+        with open("test1.xml") as test:
+            self.assertEqual(mama_papers.getLinks(ET.parse(test).getroot()),"") 
+        
+        
+if __name__ == '__main__':
+    unittest.main()
